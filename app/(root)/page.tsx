e4e72ebd7e977470ae2a2237280d2e35a3aecc58
Jishan Ahmed <workwithjishan.info@gmail.com>
2024-05-09 01:37:18 +0530

    create Header Component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,21 @@
+import HeaderBox from "@/components/HeaderBox";
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Jishan" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions effieciently"
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 export default Home;
