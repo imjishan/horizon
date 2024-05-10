3cd0045faa13ed5006a17e9047cc6ff23ec9cbe9
Jishan Laskar <workwithjishan.info@gmail.com>
2024-05-10 19:25:53 +0000

    started signin page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,4 +1,10 @@
+import AuthForm from "@/components/AuthForm";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
+  );
 };
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,4 +1,10 @@
+import AuthForm from "@/components/AuthForm";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
+  );
 };
 export default SignUp;
